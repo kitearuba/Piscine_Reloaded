9b626caf7a4274d7fcaece963cfe6492eeb7e5aa
Christian <115067566+kitearuba@users.noreply.github.com>
2024-06-10 22:33:09 +0200

    1006-2233 Update ft_strdup.c

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -13,22 +13,33 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int ft_strlen(char *str)
+{
+    int length = 0;
+    while (str[length] != '\0')
+        length++;
+    return length;
+}
+
 char	*ft_strdup(char *src)
 {
 	int		i;
 	char	*target;
+	int	length;
 
+	length = ft_strlen(src);
 	i = 0;
-	target = malloc (sizeof(char) * sizeof(src));
+	target = (char *)malloc (sizeof(char) * (length + 1));
 	if (target == NULL)
 	{
 		write(2, "Memory allocation failed.\n", 26);
-		return ("1");
+		return (NULL);
 	}
 	while (src[i] != '\0')
 	{
 		target[i] = src[i];
 		i++;
 	}
+	target[i] = '\0';
 	return (target);
 }
