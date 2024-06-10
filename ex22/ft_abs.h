10680041b46241353782be69ba9b54c624e62a59
Christian <115067566+kitearuba@users.noreply.github.com>
2024-06-10 13:09:37 +0200

    Update ft_abs.h

--- ex22/ft_abs.h
+++ ex22/ft_abs.h
@@ -10,4 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_ABS_H
+#define FT_ABS_H
+
+#define ABS(Value) ((Value) < 0 ? -(Value) : (Value))
+
+#endif // FT_ABS_H
 
