ee7ae39e19db92171d7cc7155af6a4b499d14097
Christian <115067566+kitearuba@users.noreply.github.com>
2024-06-11 14:32:35 +0200

    Update ft_count_if.c

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -12,14 +12,26 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
 
 // Function prototype for ft_count_if
 int ft_count_if(char **tab, int (*f)(char*));
 
-// Example function that checks if the string is non-empty
-int ft_check_content(char *str)
+// Function that checks if the string contains only digits
+int ft_is_digit_only(char *str)
 {
-    return (*str != '\0');
+    int i = 0;
+
+    if (str[i] == '\0')
+        return (0);
+
+    while (str[i] != '\0')
+    {
+        if (!isdigit(str[i]))
+            return (0);
+        i++;
+    }
+    return (1);
 }
 
 // Function that counts how many strings in the array return 1 when passed to f
@@ -42,7 +54,7 @@ int ft_count_if(char **tab, int (*f)(char*))
 int main(void)
 {
     char *arr[] = {"1", "", "24", "x", "12365", "e3", NULL}; 
-    int count = ft_count_if(arr, &ft_check_content);
-    printf("Count of non-empty strings: %d\n", count);
+    int count = ft_count_if(arr, &ft_is_digit_only);
+    printf("Count of digit-only strings: %d\n", count);
     return (0);
 }
