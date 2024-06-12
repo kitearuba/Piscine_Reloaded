f9c6b07aa471ea16cad8a9785e8b5ef1f37f7974
Christian Rodriguez <chrrodri@car9s5.42barcelona.com>
2024-06-12 14:31:14 +0200

    1206-1430 Must fix while loop in ft

--- ex27/v1_ft_display_file.c
+++ ex27/v1_ft_display_file.c
@@ -6,51 +6,51 @@
 /*   By: chrrodri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 19:30:56 by chrrodri          #+#    #+#             */
-/*   Updated: 2024/06/12 14:08:34 by chrrodri         ###   ########.fr       */
+/*   Updated: 2024/06/11 19:32:34 by chrrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1000
+#define BUFFER_SIZE 4096
 
 void	display_file(const char *filename)
 {
-	int			file;
-	char		buf[BUFFER_SIZE];
-	ssize_t		b_read;
+    int fd;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
 
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-	{
-		write (2, "Cannot read file.\n", 18);
-		return ;
-	}
-	b_read = read(file, buf, BUFFER_SIZE);
-	while (b_read > 0)
-	{
-		if (b_read <= 0)
-			break;
-		else
-			write(1, buf, b_read);
-	}
-	close(file);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        write(2, "Cannot read file.\n", 18);
+        return;
+    }
+
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
+    {
+        write(1, buffer, bytes_read);
+    }
+
+    close(fd);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
-	{
-		write(2, "File name missing.\n", 19);
-		return (1);
-	}
-	else if (argc > 2)
-	{
-		write(2, "Too many arguments.\n", 20);
-		return (1);
-	}
-	else
-		display_file(argv[1]);
-	return (0);
+    if (argc < 2)
+    {
+        write(2, "File name missing.\n", 19);
+        return 1;
+    }
+    else if (argc > 2)
+    {
+        write(2, "Too many arguments.\n", 20);
+        return 1;
+    }
+    else
+    {
+        display_file(argv[1]);
+    }
+    return 0;
 }
